6b8ff747eeb282d818db6e22659acec7baa51f3e
R. Saravanan <sarava@mindmeldr.com>
2012-10-06 21:23:12 -0500

    Added demo programs helloworld.sh and gsnowflake.py

---
+++ helloworld.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# helloworld.sh: a Hello World program illustrating GraphTerm escape sequences
+
+imgurl=https://github.com/mitotic/graphterm/raw/master/doc-images/helloworld1.png
+
+esc=`printf "\033"`
+graphterm_code="1155"
+echo -n "${esc}[?${graphterm_code};${GRAPHTERM_COOKIE}h"
+
+# Display text with markup
+echo '<b>Hello</b> <em style="color: red;">World!</em><p>'
+
+# Display inline image
+echo '<a href="https://github.com/mitotic/gcowsay">'
+echo '<img width="400" height="200" src="'$imgurl'"></img>'
+echo '</a>'
+
+echo -n "${esc}[?${graphterm_code}l"
