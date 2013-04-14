508337d4fa6cea9a406cf87665b48f4319d221e9
R. Saravanan <sarava@mindmeldr.com>
2013-04-13 19:10:52 -0500

    Simple demo for inline graphics using NCL

---
+++ graphterm/bin/gimg.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# gimg.sh: display PNG file inline
+
+esc=`printf "\033"`
+gterm_code="1155"
+gterm_cookie=${GRAPHTERM_COOKIE:-${LC_GRAPHTERM_COOKIE}}
+
+# Create blob
+blob_id="${RANDOM}${RANDOM}"
+echo -n "${esc}[?${gterm_code};${gterm_cookie}h"
+echo -n '<!--gterm data blob='${blob_id}'-->image/png;base64,'
+base64 $1
+echo -n "${esc}[?${gterm_code}l"
+
+# Display blob
+echo -n "${esc}[?${gterm_code};${gterm_cookie}h"
+echo -n '<!--gterm pagelet display-block blob='${blob_id}'--><div class="gterm-blockhtml"><img class="gterm-blockimg" src="/blob/local/'${blob_id}'"></div>'
+echo -n "${esc}[?${gterm_code}l"
+
